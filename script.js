cad02fe48e670450390b1334e3543a12c5b1afda
DevAnasKhalid <careernative@gmail.com>
2025-05-25 16:19:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// This function changes the heading and shows an alert
+function sayHello() {
+    const heading = document.getElementById("greeting");
+    heading.textContent = "Hello, JavaScript is working!";
+    heading.style.color = "green";
+    alert("Button clicked! JS is working.");
+}
